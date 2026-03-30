106fdc63e1ffdea42183499af925b496c63507a0
Test User <test@example.com>
2026-03-30 08:17:35 +0800

    chore: project scaffolding

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface KeyPair {
+  npub: string;
+  nsec: string;
+}
+
+export interface Contact {
+  npub: string;
+  name: string;
+}
+
+export interface ContactList {
+  contacts: Contact[];
+}
+
+export interface InboundMessage {
+  from_npub: string;
+  from_name: string | null;
+  content: string;
+  received_at: string;
+}
+
+export interface RelayStatus {
+  url: string;
+  connected: boolean;
+}
